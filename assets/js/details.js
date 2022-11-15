da198b16312b50742d783a55f44f517f92a59bb7
NoFee93 <francobrizzio@gmail.com>
2022-11-15 11:52:46 -0300

    sin carrousel, falta stats

--- assets/js/details.js
+++ assets/js/details.js
@@ -20,23 +20,44 @@ function traerDatos (url){
 
 function crearDetalles(buscarId) {
         let contenedorDetails = document.getElementById("contenedorDetails")
-        contenedorDetails.className = 'card mb-3'
-        contenedorDetails.innerHTML =`<div class="row g-0 ">
-        <div class="col-md-4 ">
-          <img src="${buscarId.image}" class="img-fluid rounded-start d-placeholder-img" style="width: 400px ; height: 200px" alt="${buscarId.name}">
-        </div>
-        <div class="col-md-8">
-          <div class="card-body">
-            <h5 class="card-title">${buscarId.name}</h5>
-            <div><span>${buscarId.category} ~ </span><span class = "fecha">${buscarId.date}</span></div>
-            
-            <div><span>Place: ${buscarId.place}</span> <span>| Capacity: <span class="numeros">${buscarId.capacity}</span></span></div>
-            <div></div>
-            <span></span>
-            <p class="card-text">${buscarId.description}</p>
-            <p class="card-text"><small class="text-muted">Price: $<span class = "numeros">${buscarId.price}</span></small></p>
+        if(buscarId.assistance !==0){
+          contenedorDetails.className = 'card mb-3'
+          contenedorDetails.innerHTML =`<div class="row g-0 ">
+          <div class="col-md-4 ">
+            <img src="${buscarId.image}" class="img-fluid rounded-start d-placeholder-img" style="width: 400px ; height: 200px" alt="${buscarId.name}">
           </div>
-        </div>
-      </div>`
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">${buscarId.name}</h5>
+              <div><span>${buscarId.category} ~ </span><span class = "fecha">${buscarId.date}</span></div>
+              <div><span>Place: ${buscarId.place}</span> <span>| Capacity: <span class="numeros">${buscarId.capacity}</span></span></div>
+              <div></div>
+              <span></span>
+              <p class="card-text">${buscarId.description}</p>
+              <p class="card-text"><small class="text-muted">Price: $<span class = "numeros">${buscarId.price}</span></small></p>
+            </div>
+          </div>
+        </div>`
+        }else{
+          contenedorDetails.className = 'card mb-3'
+          contenedorDetails.innerHTML =`<div class="row g-0 ">
+          <div class="col-md-4 ">
+            <img src="${buscarId.image}" class="img-fluid rounded-start d-placeholder-img" style="width: 400px ; height: 200px" alt="${buscarId.name}">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">${buscarId.name}</h5>
+              <div><span>${buscarId.category} ~ </span><span class = "fecha">${buscarId.date}</span></div>
+              
+              <div><span>Place: ${buscarId.place}</span> <span>| Capacity: <span class="numeros">${buscarId.capacity}</span></span></div>
+              <div></div>
+              <span></span>
+              <p class="card-text">${buscarId.description}</p>
+              <p class="card-text"><small class="text-muted">Price: $<span class = "numeros">${buscarId.price}</span></small></p>
+            </div>
+          </div>
+        </div>`
+        }
+      console.log(buscarId.assistance)
     return contenedorDetails
 }
